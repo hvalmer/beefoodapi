6bd989d3e9038474a448d43fee993fdacabdb59f
hvalmer <hvalmer@gmail.com>
2022-09-30 14:50:44 -0300

    Criacao e início da API delivery entrega de comida - BeeFood

---
+++ src/main/java/com/beefoodapi/BeefoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.beefoodapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeefoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeefoodApiApplication.class, args);
+	}
+
+}
